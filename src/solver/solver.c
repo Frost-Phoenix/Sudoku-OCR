8e4e1343fd9e2b23ca5ec938be115507283a98e4
Frost-Phoenix <67cyril6767@gmail.com>
2024-07-10 01:23:05 +0200

    intermediate functions for solve

--- src/solver/solver.c
+++ src/solver/solver.c
@@ -1,5 +1,35 @@
 #include "../include/solver/solver.h"
 
+#include <stdint.h>
+#include <stdlib.h>
+
+
+/******************************************************
+ *                 Private functions                  *
+ ******************************************************/
+
+static int _col_contains(board_t* board, size_t col, uint8_t nb) {
+    for (size_t r = 0; r < board->size; r++) {
+        if (board->cells[r][col] == nb) {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+static int _row_contains(board_t* board, size_t row, uint8_t nb) {
+    for (size_t c = 0; c < board->size; c++) {
+        if (board->cells[row][c] == nb) {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+static int _box_contains(board_t* board, size_t row, size_t col, uint8_t nb) {
+    // todo
+    return TRUE;
+}
 
 /******************************************************
  *                 Public functions                   *
